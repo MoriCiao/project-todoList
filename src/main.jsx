9b7a4bd052f2d9508f2872d6587b742f2feb3327
MoriCiao <bboynoice@gmail.com>
2025-07-12 16:09:38 +0800

    fix: Routes 修正

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/project-todoList">
     <App />
   </BrowserRouter>
 );
